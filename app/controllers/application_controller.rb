73bcc3bf118aec8e818e0ef1a1c52eb0e583b068
Xiaoyi Liu <lxy878@gmail.com>
2020-04-29 12:23:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,14 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
 
-  patch '/recipe/:id/edit'
+  patch '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect "/recipe/#{@recipe.id}"
+  end
   # display a recipe
   get '/recipes/:id' do
     @recipe = Recipe.find(params[:id])
