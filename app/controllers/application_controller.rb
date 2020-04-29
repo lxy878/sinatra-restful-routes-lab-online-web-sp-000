e29454e5fd01511be9511a062802a31b825a3146
Xiaoyi Liu <lxy878@gmail.com>
2020-04-29 11:57:04 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < Sinatra::Base
   post '/recipes' do
     new_recipe = Recipe.new(params)
     if new_recipe.save
-      raise new_recipe.id
+      binding.pry
       redirect "/recipes/#{new_recipe.id}"
     else
       erb :error
