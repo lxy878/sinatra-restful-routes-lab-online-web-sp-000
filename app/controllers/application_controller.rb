99737a3f077b08c826cf1005ea6480d5316500db
Xiaoyi Liu <lxy878@gmail.com>
2020-04-29 11:47:52 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,35 @@ class ApplicationController < Sinatra::Base
 
   # code actions here!
 
+  # create a new recipe
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    new_recipe = Recipe.new(params)
+    if new_recipe.save
+      raise new_recipe.id
+      redirect "/recipes/#{new_recipe.id}"
+    else
+      erb :error
+    end
+  end
+
+  # edit a recipe
+
+  # display all recipes
+
+  # delete a recipe
+
+  get '/recipes' do
+    erb :index
+  end
+  # display a recipe
+  get '/recipes/:id' do
+    @recipe = Recipes.find(params[:id])
+    reb :recipe
+  end
+
+
 end
